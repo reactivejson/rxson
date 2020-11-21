e7997e0666538f6ae791d058f56906f8ba3af0ed
bouhanan <mohamed.bou_hanane@nokia.com>
2020-11-21 21:57:29 +0200

    update gitignore to ignore targets

--- rxson-reactor/target/apidocs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"io.rxson.reactor","l":"ReactorProvider"}];updateSearchResults();
--- rxson-rxjava/target/apidocs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"io.rxson.rxjava","l":"RxProvider"}];updateSearchResults();
